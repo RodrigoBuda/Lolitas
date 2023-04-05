ad1408ede892c297665fb75f51bc6b82187979fe
RodrigoBuda <rodrigo.buda@outlook.com>
2023-04-04 23:00:34 -0300

    serv-galeria

--- js/data.js
+++ js/data.js
@@ -48,4 +48,53 @@ export const services = [
     image: "../img/cejass.jpg",
     price: "$1800",
   },
+  {
+    _id: 6,
+    name: "Lifting de pestañas",
+    category: "pestañas",
+    description:
+      "El lifting de pestañas es una técnica de belleza no invasiva que levanta y curva tus pestañas de forma natural. Se aplica una solución química especial que eleva las pestañas y luego se aplican tintes para realzar su color. El resultado dura de 6 a 8 semanas. En nuestro salón de belleza, ofrecemos una experiencia de spa relajante y acogedora con profesionales altamente capacitados.",
+    image: "../img/lifting.jpg",
+    imgIndex: "./img/lifting.jpg",
+    price: "$2500",
+  },
+  {
+    _id: 7,
+    name: "LipBoost",
+    category: "labios",
+    description:
+      "El servicio de LipBoost con Dermapen es una solución innovadora que combina la tecnología de seguridad LipBoost con el tratamiento de belleza Dermapen. Esta combinación única ofrece una experiencia integral de seguridad y belleza. El servicio LipBoost es una solución de seguridad que utiliza tecnología de cifrado de datos para proteger la información financiera del usuario durante las transacciones en línea. Por otro lado, el Dermapen es un dispositivo de microagujas utilizado para mejorar la salud y apariencia de la piel mediante la estimulación del colágeno y la elastina. La combinación de ambos servicios permite a los clientes experimentar una sensación de seguridad mientras se someten a un tratamiento de belleza rejuvenecedor. Al utilizar LipBoost durante el proceso de pago, los clientes pueden estar seguros de que su información financiera está protegida. Mientras tanto, el tratamiento Dermapen utiliza microagujas para estimular el colágeno y mejorar la textura, tono y apariencia de la piel. En resumen, el servicio de LipBoost con Dermapen ofrece una solución integral para aquellos que desean un tratamiento de belleza rejuvenecedor y seguro en línea. La combinación de ambos servicios garantiza la protección de la información financiera del cliente mientras se someten a un tratamiento de belleza de alta calidad.",
+    image: "../img/lipbost.jpg",
+    imgIndex: "./img/lipbost.jpg",
+    price: "$3000",
+  },
+  {
+    _id: 8,
+    name: "Limpieza Facial",
+    category: "facial",
+    description:
+      "El servicio de limpieza facial es un tratamiento de belleza que se enfoca en limpiar y purificar la piel del rostro. Este servicio se realiza utilizando técnicas de limpieza y productos especializados diseñados para eliminar la suciedad, el aceite y las impurezas de la piel, dejándola suave, radiante y rejuvenecida. La limpieza facial es un tratamiento ideal para mejorar la salud y apariencia de la piel y mantenerla en óptimas condiciones.",
+    image: "../img/faciales.jpg",
+    imgIndex: "./img/faciales.jpg",
+    price: "$2200",
+  },
+  {
+    _id: 9,
+    name: "Depilacion Con Hilo",
+    category: "cejas",
+    description:
+      "La depilación con hilo es una técnica de depilación que se originó en Asia y ha ganado popularidad en Occidente en los últimos años. Utiliza un hilo de algodón retorcido para eliminar vellos no deseados de manera precisa y natural, sin productos químicos. Es especialmente beneficiosa para dar forma y definir las cejas y tiene resultados más duraderos y menos irritantes que la depilación con cuchilla o cera. Es una técnica natural y precisa que se ha utilizado durante siglos y ofrece una alternativa a la depilación con cera o cuchilla.",
+    image: "../img/hilo.jpg",
+    price: "$1400",
+  },
+
+  {
+    _id: 10,
+    name: "Cejas HD",
+    category: "cejas",
+    description:
+      "El servicio de cejas HD es una técnica especializada de diseño y definición de cejas que se enfoca en crear cejas de aspecto natural, definido y simétrico. Este servicio utiliza técnicas de depilación y maquillaje para dar forma y definir las cejas, utilizando productos de alta calidad para lograr un aspecto duradero y resistente al agua. Las cejas HD son ideales para aquellas personas que desean una apariencia definida y natural de las cejas, y puede ser adaptado a diferentes formas y estilos de cejas según las necesidades y preferencias del cliente.",
+    image: "../img/cejass.jpg",
+    price: "$1800",
+  },
 ];
--- js/servicios.js
+++ js/servicios.js
@@ -4,14 +4,14 @@ const contServicios = document.getElementById("contServicios");
 function mostrarServicios(servicios) {
   let tarjetas = "";
   for (const s of servicios) {
-    tarjetas += `<div class="card">
+    tarjetas += `
+         <div class="card">
           <img class="tarjetas-img" src="${s.image}" alt="" />
           <div class="card-body">
             <h3 class="card-title"><b>${s.name}</b></h3>
             <p class="card-text p-description"> ${s.description} </p>
             <h5 > <b>...</b></h5>
             <button id="botonJs" type="button" class="boton" onclick="window.location.href='../pages/servicio.html?id=${s._id}';">Mas Detalles</button>
-
           </div>
         </div>`;
   }
